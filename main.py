12600b3590a283c5059cedef26785e6ae8b28756
Michael Steiner <michaelsteiner@Michaels-MacBook-Pro.local>
2021-01-12 20:32:36 -0800

    ip adress and basic display

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ print("Clear...")    # prints to console, not the display, for debugging
 epd.Clear(0xff)
 
 def get_ip_address():
- ip_address = ''
- s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
- s.connect(("8.8.8.8",80))
- ip_address = s.getsockname()[0]
- s.close()
- return ip_address
+    ip_address = ''
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8",80))
+    ip_address = s.getsockname()[0]
+    s.close()
+    return ip_address
 
 def printToDisplay(string):
     HBlackImage = Image.new('1', (epd2in7.EPD_HEIGHT, epd2in7.EPD_WIDTH), 255)
